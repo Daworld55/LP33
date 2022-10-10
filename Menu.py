5ee953f6fffd1066dbb8675ff15483b2e4353251
Daworld55 <115400401+Daworld55@users.noreply.github.com>
2022-10-10 14:57:26 +0600

    Add files via upload

---
+++ Menu.py
@@ -0,0 +1,18 @@
+def menu():
+        while True: 
+                print("1. Кафедра ТК") 
+                print("2. Факультет") 
+                print("3. Каферда Радиотехника") 
+                print("4. Выйти") 
+                ch = input("Выберите пункт: ") 
+ 
+                if ch == "1": 
+                    print("Выбран 1 пункт") 
+                elif ch == "2": 
+                    print("Выбран 3 пункт") 
+                elif ch == "3": 
+                    print("Выбран 3 пункт") 
+                elif ch == "4": 
+                    break 
+                else: 
+                    print("Такого нет")
---
+++ MiniCalculator.py
@@ -0,0 +1,17 @@
+def MiniCalculator (): 
+    import math 
+    print("Мини калькулятор") 
+    print("введите число1:") 
+    Number1 = int(input()) 
+    print("введите число2:") 
+    Number2 = int(input()) 
+    print("введите знак:") 
+    sign = str(input()) 
+    if(sign=='+'): 
+        print(Number1+Number2) 
+    elif(sign=='-'): 
+        print(Number1-Number2) 
+    elif(sign=='*'): 
+        print(Number1*Number2) 
+    elif(sign=='/'): 
+        print(Number1/Number2) 
